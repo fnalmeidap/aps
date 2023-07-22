2521c42944c1d752c43245d71671e242de8d405c
Samuel Simões <samuelsimoes31@gmail.com>
2023-07-22 11:19:42 -0300

    feat: base for login page

--- SistemaOlimpo/SistemaOlimpo/ClientApp/src/routes/AppRoutes.jsx
+++ SistemaOlimpo/SistemaOlimpo/ClientApp/src/routes/AppRoutes.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import { Counter } from "../pages/Counter";
 import { FetchData } from "../pages/FetchData";
 import { Home } from "../pages/Home";
+import Login from '../pages/Login';
 
 const AppRoutes = [
   {
     index: true,
+    element: <Login />
+  },
+  {
+    path: '/home',
     element: <Home />
   },
   {
