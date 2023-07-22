d27f4da8a1d09f973710a53bfc2e2bbec77e1584
Samuel Simões <samuelsimoes31@gmail.com>
2023-07-22 13:47:20 -0300

    feat: testing google login

--- SistemaOlimpo/SistemaOlimpo/ClientApp/src/pages/TelaCadastro.jsx
+++ SistemaOlimpo/SistemaOlimpo/ClientApp/src/pages/TelaCadastro.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { GoogleLogin } from 'react-google-login'
+
 
 export function TelaCadastro() {
+
+  const responseGoogle = (response) => {
+    console.log(response);
+  }
+
   return (
-    <div>TelaCadastro</div>
+    <div>
+      <GoogleLogin
+        clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+        buttonText='Cadastre-se com Google'
+        cookiePolicy='single_host_origin'
+        onSuccess={responseGoogle}
+        // isSignedIn={true}
+      />
+    </div>
   )
 }
